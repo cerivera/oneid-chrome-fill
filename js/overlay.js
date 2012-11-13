de2fc575ca5c6ba3e0834e30c68962e8d6bb3a2a
Carlos Rivera <riverautx@gmail.com>
2012-11-12 23:52:20 -0600

    added blue sidebar

--- js/overlay.js
+++ js/overlay.js
@@ -1,17 +1,33 @@
 var $ = jQuery;
 var OVERLAY_ID = 'ocrx-overlay';
+var SIDEBAR_ID = 'ocrx-sidebar';
 
 function removeOverlay() {
     $('#'+OVERLAY_ID).remove();
 }
 
-function launchOverlay() {
-    removeOverlay();
+function showOverlay() {
     $('<div></div>')
         .attr("id",OVERLAY_ID)
         .attr("class", OVERLAY_ID)
         .appendTo('body');
 }
 
-launchOverlay();
+function showSidebar() {
+    $('<div></div>')
+        .attr("id",SIDEBAR_ID)
+        .attr("class", SIDEBAR_ID)
+        .appendTo('#' + OVERLAY_ID);
+}
+
+function startApp() {
+    if ($('#'+OVERLAY_ID).length) {
+        removeOverlay();
+    } else {
+        showOverlay();
+        showSidebar();
+    }
+}
+
+startApp();
 
