cf419b851a81dc4c5262c08281edf24cf6c15479
Carlos Rivera <riverautx@gmail.com>
2012-11-13 15:39:07 -0600

    jquery-ui and added draggables

--- js/overlay.js
+++ js/overlay.js
@@ -15,9 +15,50 @@ function showOverlay() {
 
 function showSidebar() {
     $('<div></div>')
+        .html(getHtmlForDraggables())
         .attr("id",SIDEBAR_ID)
         .attr("class", SIDEBAR_ID)
         .appendTo('#' + OVERLAY_ID);
+}
+
+function getHtmlForDraggables() {
+    var html = '<div class="ocrx-draggables">'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Full name</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>First name</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Last name</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Telephone</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Email</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Street 1</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Street 2</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>City</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>State</p>'
+        + '</div>'
+        + '<div class="ocrx-draggable ui-widget-content">'
+        + '<p>Zip</p>'
+        + '</div>'
+        + '</div>';
+    return html;
+}
+
+function initDraggable() {
+    $( ".ocrx-draggable" ).draggable();
 }
 
 function startApp() {
@@ -26,6 +67,7 @@ function startApp() {
     } else {
         showOverlay();
         showSidebar();
+        initDraggable();
     }
 }
 
